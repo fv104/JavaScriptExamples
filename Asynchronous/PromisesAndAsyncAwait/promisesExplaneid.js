8a4524e13dec02c91f50667ce38a5bca2af51ef3
Felix <fvicenta@capgemini.com>
2022-02-01 16:55:18 +0100

    Continuacion de ejercicios

--- Asynchronous/PromisesAndAsyncAwait/promisesExplaneid.js
+++ Asynchronous/PromisesAndAsyncAwait/promisesExplaneid.js
@@ -15,11 +15,19 @@ const citiesList = {
     4: "Ciudad Real"
 }
 
-function getUsers(){
+function getUsers(dato,intervalo){
     return new Promise((resolve,reject) =>{
-        setTimeout(() => {
-            resolve(usersList)
-        }, 3000);
+
+        if(dato == '' || typeof dato !== 'object'){
+            reject("El dato esta vacio o no es el objeto")
+        }else if(intervalo < 0 || typeof intervalo !== 'number'){
+            reject("El intervalo no puede ser negativo y tiene que ser numericoo")
+        }else{
+            setTimeout(() => {
+                resolve(dato)
+            }, intervalo);
+        }
+        
     })
 }
 
@@ -27,6 +35,40 @@ function getCities(){
     return new Promise((resolve,reject) =>{
         setTimeout(() => {
             resolve(citiesList)
-        }, 1000);
+        }, 2000);
     })
 }
+
+//Para consumir las promesas
+
+const usersPromise = getUsers(usersList,3000);
+const citiesPromise = getCities()
+
+// usersPromise.then(users => console.log(users))
+// citiesPromise.then(cities => console.log(cities))
+
+
+
+//Las promesas se pueden encadenar, para garantizar que su procesamiento sea en un order correcto
+
+// usersPromise.then(users =>{
+//     console.log("Users ->", users)
+//     return getCities()
+// }).then(cities =>{
+//     console.log("Cities ->",cities)
+// }).catch(e =>{
+//     console.log("Se ha producido un error")
+// })
+
+/** Async Await, implica "No more then". Permite tratar el codigo asincrono con una apariencia sincronica
+ * porque internamente se continua con las promesas
+ */
+
+const getInfo = async () =>{
+    let users = await getUsers(usersList,3000)
+    let cities = await getCities()
+    console.log("Users ->", users)
+    console.log("Cities ->",cities)
+}
+
+getInfo();
